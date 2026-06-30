9c172090183baddfb9111393c2403f53227ab533
wesleyfelix <wesley.felixdelima96@hotmail.com>
2026-06-30 20:08:03 -0300

    refatoração para uso do databind e ajustes no código após as refatorações

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,14 +2,43 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    public Abrigo(){
+
+    }
+
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
     }
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPets(Pet[] pets) {
+        this.pets = pets;
+    }
 }
